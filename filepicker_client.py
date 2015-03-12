a577e1e37ddbd1a6a6122e810d3047c2f0dc1664
Bartosz Kwiecień <b.kwiecien@gmail.com>
2015-03-12 15:34:08 +0100

    Added storing options

--- filepicker_client.py
+++ filepicker_client.py
@@ -2,9 +2,10 @@ import requests
 import mimetypes
 import json
 
+
 from filepicker_file import FilepickerFile
 
-class Filepicker(object):
+class FilepickerClient(object):
 
     API_URL = 'https://www.filepicker.io/api'
 
@@ -23,16 +24,18 @@ class Filepicker(object):
     def store_from_url(self, url, **kwargs):
         data = {'url': url}
         store = kwargs.get('store', self.store)
-        return self.__post(store, data=data)
+        return self.__post(store, data=data, **kwargs)
 
     def store_local_file(self, filepath, **kwargs):
         files = {'fileUpload': open(filepath, 'rb')}
         payload = {'mimetype': mimetypes.guess_type(filepath)}
         store = kwargs.get('store', self.store)
-        return self.__post(store, data=payload, files=files)
+        return self.__post(store, data=payload, files=files, **kwargs)
 
     def __post(self, store, **post_data):
         post_url = "%s/store/%s?key=%s" % (self.API_URL, store, self.api_key)
+        options = self.__storing_options(**post_data)
+        post_url += options
 
         self.__debug_msg('Post URL: %s\nPOST data: %s' % \
                 (post_url, post_data))
@@ -44,9 +47,19 @@ class Filepicker(object):
                    )
         try:
             response_dict = json.loads(response.text)
-            return FilepickerFile(response_dict)
+            return FilepickerFile(response_dict=response_dict,
+                                  api_key=self.api_key)
         except ValueError:
             return response
+
+    def __storing_options(self, **options):
+        result = ''
+        supported_options = ['filename',  'mimetype', 'path',
+                             'container', 'access',   'base64decode']
+        for option in supported_options:
+            if options.get(option, False):
+                result += '&%s=%s' % (option, options[option])
+        return result
 
     def __debug_msg(self, msg):
         if self.debug:
--- filepicker_file.py
+++ filepicker_file.py
@@ -1,31 +1,47 @@
 import requests
 import json
+import re
+
 
 class FilepickerFile(object):
 
-    FILE_API_URL = 'https://www.filepicker.io/api/file'
+    FILE_API_URL = 'https://www.filepicker.io/api/file/'
 
-    def __init__(self, response_dict=None, **kwargs):
+    def __init__(self, handle=None, url=None, response_dict=None, api_key=None):
         self.metadata = None
-        if response_dict:
+        if handle:
+            self.__init_with_handle_or_url(handle=handle)
+        elif url:
+            self.__init_with_handle_or_url(url=url)
+        elif response_dict:
             self.__init_with_dict(response_dict)
-        elif 'handle' in kwargs or 'url' in kwargs:
-            self.__init_with_handle_or_url(**kwargs)
         else:
-            raise TypeError('Please provide response_dict, handle or url')
+            raise AttributeError('Please provide file handle or url')
 
-    def __str__(self):
-        return self.url
+        self.handle = handle if handle else self.__get_handle()
+        self.set_api_key(api_key)
+
+    #def __str__(self):
+    #    return self.url
 
     def __init_with_dict(self, d):
         for key in d:
             self.__setattr__(key, d[key])
 
-    def __init_with_handle_or_url(self, **kwargs):
-        self.url = kwargs.get('url', "%s/%s" % \
-                (self.FILE_API_URL, kwargs['handle']))
+    def __init_with_handle_or_url(self, handle=None, url=None):
+        if handle:
+            self.url = self.FILE_API_URL + handle
+        elif url:
+            self.url = url
         self.update_metadata()
         self.__init_with_dict(self.metadata)
+
+    def __get_handle(self):
+        h = re.search(r'file/(\w+)', self.url).group(1)
+        return h
+
+    def set_api_key(self, api_key):
+        self.api_key = api_key
 
     def update_metadata(self):
         response = requests.get(self.url + '/metadata')
@@ -36,8 +52,9 @@ class FilepickerFile(object):
             return False
 
     def delete(self):
-        response = requests.delete(self.url)
-        return {response['ok'], response['reason']}
+        if self.api_key is None:
+            return "Please set API key first"
+        return requests.delete(self.url + '?key=' + self.api_key)
 
     def download(self, destination_path):
         with open(destination_path, 'wb') as f:
@@ -47,7 +64,7 @@ class FilepickerFile(object):
                     if not chunk:
                         break
                     f.write(chunk)
-            return {response['ok'], response['reason']}
+            return response
 
     def __getattribute__(self, name):
         CASHABLE_ATTRS = ['metadata']
