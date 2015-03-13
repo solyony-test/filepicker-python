2166ce6e9f90a50f8514406b789eaa155ffdff6c
Bartosz Kwiecień <b.kwiecien@gmail.com>
2015-03-13 16:44:21 +0100

    Added overwrite() and convert() methods

--- filepicker_client.py
+++ filepicker_client.py
@@ -1,7 +1,7 @@
-import requests
 import mimetypes
 import json
 
+import requests
 
 from filepicker_file import FilepickerFile
 
@@ -33,12 +33,13 @@ class FilepickerClient(object):
         return self.__post(store, data=payload, files=files, **kwargs)
 
     def __post(self, store, **post_data):
-        post_url = "%s/store/%s?key=%s" % (self.API_URL, store, self.api_key)
+        post_url = '{}/store/{}?key={}'.format(
+                   self.API_URL, store, self.api_key)
         options = self.__storing_options(**post_data)
         post_url += options
 
-        self.__debug_msg('Post URL: %s\nPOST data: %s' % \
-                (post_url, post_data))
+        self.__debug_msg('POST url: {}\nPOST data: {}'.format(
+                             post_url, post_data))
 
         response = requests.post(
                        post_url,
@@ -58,7 +59,7 @@ class FilepickerClient(object):
                              'container', 'access',   'base64decode']
         for option in supported_options:
             if options.get(option, False):
-                result += '&%s=%s' % (option, options[option])
+                result += '&{}={}'.format(option, options[option])
         return result
 
     def __debug_msg(self, msg):
--- filepicker_file.py
+++ filepicker_file.py
@@ -1,14 +1,19 @@
-import requests
+import mimetypes
 import json
 import re
+
+import requests
 
 
 class FilepickerFile(object):
 
     FILE_API_URL = 'https://www.filepicker.io/api/file/'
 
-    def __init__(self, handle=None, url=None, response_dict=None, api_key=None):
+    def __init__(self, handle=None, url=None, response_dict=None,
+                 api_key=None, **kwargs):
+
         self.metadata = None
+        self.converted = kwargs.get('converted', False)
         if handle:
             self.__init_with_handle_or_url(handle=handle)
         elif url:
@@ -25,6 +30,8 @@ class FilepickerFile(object):
     #    return self.url
 
     def __init_with_dict(self, d):
+        if self.converted:
+            return
         for key in d:
             self.__setattr__(key, d[key])
 
@@ -37,21 +44,25 @@ class FilepickerFile(object):
         self.__init_with_dict(self.metadata)
 
     def __get_handle(self):
-        h = re.search(r'file/(\w+)', self.url).group(1)
-        return h
+        return re.search(r'file/(\w+)', self.url).group(1)
 
     def set_api_key(self, api_key):
+        """
+        Set API key for FilepickerFile object
+        """
         self.api_key = api_key
 
     def update_metadata(self):
         response = requests.get(self.url + '/metadata')
         try:
             self.metadata = json.loads(response.text)
-            return True
         except ValueError:
-            return False
+            self.metadata = {}
 
     def delete(self):
+        """
+        Delete file. Returns requests.Response object.
+        """
         if self.api_key is None:
             return "Please set API key first"
         return requests.delete(self.url + '?key=' + self.api_key)
@@ -65,6 +76,52 @@ class FilepickerFile(object):
                         break
                     f.write(chunk)
             return response
+
+    def overwrite(self, url=None, path=None):
+        data, files = None, None
+        if url:
+            data = {'url': url}
+        if path:
+            files = {'fileUpload': open(path, 'rb')}
+            data = {'mimetype': mimetypes.guess_type(path)}
+        return self.__post(self.url, data=data, files=files)
+
+    def convert(self, **options):
+        storing_options = ['filename', 'storeLocation', 'storePath',
+                           'storeContainer', 'storeAccess']
+
+        conversions = ['w', 'h', 'fit', 'crop', 'allign', 'format',
+                       'filter', 'blurAmount', 'sharpenAmount',
+                       'compress', 'quality', 'rotate',
+                       'watermark', 'watersize', 'waterposition']
+        c_opts = []
+        for c in conversions:
+            if options.get(c, False):
+                c_opts.append('{}={}'.format(c, options[c]))
+
+        s_opts = []
+        for o in storing_options:
+            if options.get(o, False):
+                s_opts.append('{}={}'.format(o, options[o]))
+
+        if s_opts:
+            if self.api_key is None:
+                return "Please set API key first"
+            url = '{}/convert?{}&{}?key={}'.format(self.url, '&'.join(c_opts),
+                               '&'.join(s_opts), self.api_key)
+            return self.__post(url)
+        url = '{}/convert?{}'.format(self.url, '&'.join(c_opts))
+        return FilepickerFile(url=url, converted=True)
+
+    def __post(self, url, **kwargs):
+        try:
+            r = requests.post(url,
+                              data=kwargs.get('data'),
+                              files=kwargs.get('files'))
+            rd = json.loads(r.text)
+            return FilepickerFile(response_dict=rd, api_key=self.api_key)
+        except requests.exceptions.ConnectionError as e:
+            raise e
 
     def __getattribute__(self, name):
         CASHABLE_ATTRS = ['metadata']
